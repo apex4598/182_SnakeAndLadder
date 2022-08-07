beee1fd689458df2021f38fbf1f9f4cf0feb131f
Apex4598 <login2ajit@gmail.com>
2022-08-08 04:26:14 +0530

    Added : Start Game Functionality

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game.");
+        int startPosition=0;
     }
 
 }

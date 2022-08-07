d053ddc5c6a3d0191faf6861b7b6ba8bd8d60577
Apex4598 <login2ajit@gmail.com>
2022-08-08 05:20:43 +0530

    Added :  Functionality to Check Winning Position

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -6,12 +6,33 @@ public class SnakeAndLadder {
     public static final int SNAKE = 2;
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake And Ladder Game.");
+        System.out.println("Welcome to Snake And Ladder game.");
 
-        int startPosition=0;
+        int startPosition = 0;
+        int Position = 0;
+        int newPosition = 0;
+        int winPositiom = 100;
 
-        int DieRolled = (int) (Math.random() * 6) + 1;
-            
+        while (Position <= winPositiom) {
+            int DieRolled = (int) (Math.random() * 6) + 1;
+            int checkOption = (int) (Math.random() * 3);
+
+
+            switch (checkOption) {
+                case NO_PLAY:
+                    newPosition = 0;
+                    break;
+                case LADDER:
+                    newPosition = DieRolled;
+                    break;
+                case SNAKE:
+                    newPosition = -DieRolled;
+            }
+            Position = Position + newPosition;
+            if (Position < 0) {
+                Position = 0;
+            }
+            System.out.println("You are now at " + Position + "th" + " position");
+        }
     }
-
 }

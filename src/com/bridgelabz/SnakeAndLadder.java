7537c8b96edfac8fef6b312904d45b8dbc9a6c14
Apex4598 <login2ajit@gmail.com>
2022-08-08 04:47:44 +0530

    Added Functionality: To rolls the Die to get Number

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -9,33 +9,9 @@ public class SnakeAndLadder {
         System.out.println("Welcome to Snake And Ladder Game.");
 
         int startPosition=0;
-        int Position = 0;
-        int newPosition = 0;
-        int winPositiom = 100;
 
-        while (Position <= winPositiom) {
             int DieRolled = (int) (Math.random() * 6) + 1;
-            int checkOption = (int) (Math.random() * 3);
-
-            switch (checkOption) {
-                case NO_PLAY:
-                    newPosition = 0;
-                    break;
-                case LADDER:
-                    newPosition = DieRolled;
-                    break;
-                case SNAKE:
-                    newPosition = -DieRolled;
-            }
-            Position = Position + newPosition;
-            if (Position < 0) {
-                Position = 0;
-            }
-            if (Position > 100){
-                Position = newPosition;
-            }
-            System.out.println("You are now at " + Position + "th" + " position");
-        }
+            
     }
 
 }
